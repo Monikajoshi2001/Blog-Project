c104225e3fd42a73c231dd51d068bba2995ef33c
monika <monikaj@yamaha-motor-india.com>
2024-05-07 14:03:44 +0530

    Designed Components

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutButton from "./header/LogoutButton";
+import Button from "./Button";
+import Input from "./Input";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutButton,
+    Button,
+    Input
+}
